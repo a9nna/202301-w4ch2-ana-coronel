d1677aeade1fcd6f19b3f7a1dac9f5b867b7d278
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-31 20:39:50 +0100

    Add ErrorDisplay component

---
+++ src/components/ErrorDisplay/ErrorDisplay.tsx
@@ -0,0 +1,9 @@
+const ErrorDisplay = (): JSX.Element => {
+  return (
+    <p className="error text-danger col">
+      Error: I couldn't retrieve anything funny
+    </p>
+  );
+};
+
+export default ErrorDisplay;
