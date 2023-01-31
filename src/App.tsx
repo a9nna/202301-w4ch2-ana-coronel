614eee3d65b4ba6429ec2a3517571fe2748744d5
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-31 19:26:39 +0100

    Add model structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="container">
+      <form className="search-form">
+        <div className="row">
+          <label htmlFor="search" className="col-1 col-form-label">
+            Search:{" "}
+          </label>
+          <div className="col-4">
+            <input type="text" id="search" className="search form-control" />
+          </div>
+          <button type="submit" className="btn btn-info btn-sm col-1">
+            Go
+          </button>
+        </div>
+      </form>
+      <div className="row">
+        <div className="container-gif offset-1 col-6">
+          <img src="" alt="" className="gif" />
+        </div>
+      </div>
+      <div className="row">
+        <p className="error text-danger col">
+          Error: I couldn't retrieve anything funny
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
